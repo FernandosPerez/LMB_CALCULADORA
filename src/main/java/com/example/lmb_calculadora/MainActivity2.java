a31fc65f58a10d3e88d39a82744f617b0efa577c
FernandoPérez <fernandoestrp@outlook.es>
2022-05-16 02:56:45 -0500

    integración de botones numericos y botones de operación, caja de texto para visualizar valores y corrección de diseño

--- src/main/java/com/example/lmb_calculadora/MainActivity2.java
+++ src/main/java/com/example/lmb_calculadora/MainActivity2.java
@@ -3,6 +3,9 @@ package com.example.lmb_calculadora;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +13,24 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+
+
+
+
     }
+
+    public int suma (int a , int b){
+        return a + b;
+    }
+    public int resta (int a , int b){
+        return a - b;
+    }
+    public int div (int a , int b){
+        return a * b;
+    }
+    public int mult (int a , int b){
+        return a / b;
+    }
+
 }

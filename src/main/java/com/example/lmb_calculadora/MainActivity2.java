2ef248dbc77b42426634b8891e7b04326d028cf7
FernandoPérez <fernandoestrp@outlook.es>
2022-05-15 23:08:57 -0500

    avance de la segunda interfaz

---
+++ src/main/java/com/example/lmb_calculadora/MainActivity2.java
@@ -0,0 +1,14 @@
+package com.example.lmb_calculadora;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}

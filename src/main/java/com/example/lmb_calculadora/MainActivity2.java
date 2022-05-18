02bc79f8073d40826121f08d600cd7e63fd19b2e
FernandoPérez <fernandoestrp@outlook.es>
2022-05-17 19:09:40 -0500

    actualización de la build para poder compartir proyecto con version compatible de la ardillita

--- src/main/java/com/example/lmb_calculadora/MainActivity2.java
+++ src/main/java/com/example/lmb_calculadora/MainActivity2.java
@@ -5,16 +5,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity2 extends AppCompatActivity {
 
+    Button suma;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
+    suma=(Button) findViewById(R.id.suma);
+    suma.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
 
+            Toast.makeText(getApplicationContext(),"suma",Toast.LENGTH_LONG).show();
+
+        }
+    });
 
 
 
